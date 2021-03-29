ce19b56e27250a1cfcbdf76ab29ff0bce78b3681
Sam <samleitch75@gmail.com>
2021-03-29 21:41:05 +1100

    Tweeks to player controller

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -6,19 +6,44 @@ public class ThirdPersonController : MonoBehaviour
 {
     public CharacterController controller;
     public Transform camera;
+    public Transform groundCheck;
 
+    public LayerMask groundMask;
+
+    public float jumpPower = 3f;
+    public float groundDistance = 0.4f;
     public float speed = 6f;
-
+    public float gravity = -9.81f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    Vector3 velocity;
+    bool isGrounded = true;
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        //jump power calculation
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpPower * -2f * gravity);
+        }
+
+        //adding gravity
+        velocity.y += gravity + Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (direction.magnitude >= 0.1f)
         {
             //mathamatical function, figures out what andgle to move towards
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
@@ -27,7 +52,7 @@ public class ThirdPersonController : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);            
         }
     }
 }
