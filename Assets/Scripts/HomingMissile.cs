87ef2e9fe614f9b768fb7b786098f83567624486
Sam <samleitch75@gmail.com>
2021-04-19 16:02:38 +1000

    minor changes

---
+++ Assets/Scripts/HomingMissile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingMissile : MonoBehaviour
+{
+    ////find player coming first
+    //private void Awake()
+    //{
+    //    find playerInTheLead
+
+    //}
+
+    //private void Update()
+    //{
+    //    Vector3 angleVector = playerInTheLead.transform.position - transform.position;
+    //    float targetRotation = Mathf.Rad2Deg * Mathf.Atan2(angleVector.y, angleVector.x) - 90.0f;
+    //    Quaternion finalRotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(targetRotation, Vector3.forward), rotation * Time.deltaTime);
+    //    transform.rotation = finalRotation;
+    //}
+
+
+
+}
